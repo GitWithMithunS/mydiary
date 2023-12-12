6b8bf7ab43c651c277313f8cc3b533a821f71332
Mithun s <chakravarthimithun62@gmail.com>
2023-12-13 00:13:49 +0530

    'contextapisnippet'

--- src/About/About.js
+++ src/About/About.js
@@ -1,14 +1,18 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useContext } from 'react'
 import notecontext from '../context/notes/NoteContext'
 
 export default function About() {
   const a = useContext(notecontext)
+  useEffect(() => {
+    a.update()
+    // eslint-disable-next-line
+  },[])
   return (
     <>
       <div>All About Me</div>
       <div>
-        this is about {a.name} of class {a.class}
+        this is about {a.state.name} of class {a.state.class}
       </div>
     </>
    
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -1,13 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import notecontext from "./NoteContext";
 
 const NoteState = (props) => {
-    const state = {
+    const s1 = {
         "name":"mithun",
         "class":"3bsec"
     }
+    const [state,setstate] = useState(s1)
+    const update = () => {
+        setTimeout(() => {
+            setstate({
+                "name":"chakravarthi",
+                "class":"2csec"
+            },5000)
+        },5000)
+    }
     return(
-        <notecontext.Provider value={state}>      
+        <notecontext.Provider value={{state,update}}>      {/*value={{state:state,update:update}} it can be writtten in both this ways  */} 
             {props.children}
         </notecontext.Provider>
     )
